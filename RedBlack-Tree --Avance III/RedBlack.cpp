eaea5a6c22b592a7ca7fc85db3ee571c6d5a9828
Jhoel <jhuallpard@ulasalle.edu.pe>
2019-05-10 11:07:49 -0500

    Corregido

--- RedBlack-Tree --Avance III/RedBlack.cpp
+++ RedBlack-Tree --Avance III/RedBlack.cpp
@@ -1,8 +1,6 @@
 #include "RedBlack.h"
 #include <iostream>
 #include <fstream>
-#include <bits/stdc++.h>
-#include <time.h>
 
 using namespace std;
 
@@ -10,40 +8,13 @@ int main()
 {
     redblack<int, int> rb;
     int iSecret, jSecret;
-    //srand (time(NULL));
 
     for(int i=1;i<10;i++)
-	{
-       
+	{ 
         rb(i) = i;
-        //rb.print_arbol(i);
     }
     
     rb.print_arbol(20);
-
-
     return 0;
 }
 
-/*
-int main()
-{
-    
-    redblack<int,int> avl;
-
-    //int iSecret, jSecret;
-    //srand (time(NULL));
-
-    for(int i=1;i<30;i++){
-        //iSecret = rand() % 30 + 1;
-        //redblack.insert(i,1);
-        redblack.insert(1,2,1,1,1,1);
-            //cout<<"Se inserto "<<iSecret<<endl;
-            //avl.print_arbol(iSecret);
-    }
-    redblack.print_arbol(21);
-       
-
-	return 0;
-}
-*/
